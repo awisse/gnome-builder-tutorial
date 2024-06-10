e11b688329dcd9e175d6a9002b8365341a15e136
Aurel Wisse <4907846+awisse@users.noreply.github.com>
2024-06-10 13:40:38 -0400

    Gnome Builder Tutorial Step 4: Showing the Cursor Position

--- src/text-viewer-window.c
+++ src/text-viewer-window.c
@@ -29,9 +29,10 @@ struct _TextViewerWindow
   GSettings           *settings;
 
 	/* Template widgets */
-	AdwHeaderBar        *header_bar;
-	GtkTextView         *main_text_view;
-  GtkButton           *open_button;
+	AdwHeaderBar *header_bar;
+	GtkTextView *main_text_view;
+  GtkButton *open_button;
+  GtkLabel *cursor_pos;
 };
 
 G_DEFINE_FINAL_TYPE (TextViewerWindow, text_viewer_window, ADW_TYPE_APPLICATION_WINDOW)
@@ -45,12 +46,18 @@ text_viewer_window_class_init (TextViewerWindowClass *klass)
 	gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, header_bar);
 	gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, main_text_view);
 	gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, open_button);
+	gtk_widget_class_bind_template_child (widget_class, TextViewerWindow, cursor_pos);
 }
 
 static void
 text_viewer_window__open_file_dialog (GAction             *action,
                                       GVariant            *parameter,
                                       TextViewerWindow    *self);
+
+static void
+text_viewer_window__update_cursor_position (GtkTextBuffer *buffer, 
+                                            GParamSpec *pspec, 
+                                            TextViewerWindow *self);
 
 static void
 text_viewer_window_init (TextViewerWindow *self)
@@ -61,6 +68,11 @@ text_viewer_window_init (TextViewerWindow *self)
   g_signal_connect (open_action, "activate", 
       G_CALLBACK (text_viewer_window__open_file_dialog), self);
   g_action_map_add_action (G_ACTION_MAP (self), G_ACTION (open_action));
+
+  GtkTextBuffer *buffer = gtk_text_view_get_buffer (self->main_text_view);
+  g_signal_connect (buffer, "notify::cursor-position",
+                    G_CALLBACK (text_viewer_window__update_cursor_position),
+                    self);
 }
 
 static void
@@ -192,4 +204,27 @@ open_file_complete (GObject           *source_object,
   gtk_window_set_title (GTK_WINDOW (self), display_name);
 }
 
-// vim: ts=2:sts=2:expandtab 
+static void
+text_viewer_window__update_cursor_position (GtkTextBuffer *buffer, 
+                                            GParamSpec *pspec, 
+                                            TextViewerWindow *self)
+{
+  int cursor_pos = 0;
+
+  // Retrieve the value of the "cursor-position" property
+  g_object_get (buffer, "cursor-position", &cursor_pos, NULL);
+
+  // Construct the text iterator for the position of the cursor
+  GtkTextIter iter;
+  gtk_text_buffer_get_iter_at_offset (buffer, &iter, cursor_pos);
+
+  // Set the new contents of the label
+  g_autofree char *cursor_str =
+    g_strdup_printf ("Ln %d, Col %d",
+                     gtk_text_iter_get_line (&iter) + 1,
+                     gtk_text_iter_get_line_offset (&iter) + 1);
+
+  gtk_label_set_text (self->cursor_pos, cursor_str);
+}
+
+// vim: ts=2:sts=2:sw:expandtab 
